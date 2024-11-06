ef1aa202a10af36e566adcdf0a17693b9fe050d1
pompo <riroldo3@gmail.com>
2024-11-06 18:28:43 +0100

    Generate Scaffold

---
+++ src/main/java/com/vivelibre/tokenController/TokenControllerApplication.java
@@ -0,0 +1,13 @@
+package com.vivelibre.tokenController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivelibre/tokenController/TokenControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivelibre.tokenController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
